9eef5b517bc5e923e218eea489a46db69043c80e
Janelle Zorvan <jzorvan@gmail.com>
2017-03-06 09:00:14 -0600

    Partial Script

--- 09_14_Must_Know_Dev_Tools_Tricks/script.js
+++ 09_14_Must_Know_Dev_Tools_Tricks/script.js
@@ -9,18 +9,26 @@ const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
     }
 
     // Regular
+    console.log('Hello.');
 
     // Interpolated
+    console.log('Hello, I am a %s person!', 'baddass');
 
     // Styled
+    console.log('%c I am some great text.', 'color: purple; background-color: green');
 
     // warning!
+    console.warn('Oh no!!');
 
     // Error :|
+    console.error('There is an error.');
 
     // Info
+    console.info('This is useful info.');
 
     // Testing
+    const p = document.querySelector('p');
+    console.assert(p.classList.contains('zibbityza'), 'That is wrong.');
 
     // clearing
 
