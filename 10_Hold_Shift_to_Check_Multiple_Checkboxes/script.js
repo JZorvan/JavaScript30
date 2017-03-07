7fb9f6faa0e770de47e97d40983f6c449adc4c33
Janelle Zorvan <jzorvan@gmail.com>
2017-03-06 23:03:44 -0600

    Finished Script

--- 10_Hold_Shift_to_Check_Multiple_Checkboxes/script.js
+++ 10_Hold_Shift_to_Check_Multiple_Checkboxes/script.js
@@ -1,2 +1,22 @@
 'use strict';
 
+const boxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(e) {
+    let inBetween = false;
+    if (e.shiftKey && this.checked) {
+        boxes.forEach(box => {
+            if (box === this || box === lastChecked) {
+                inBetween = !inBetween;
+            }
+            if (inBetween) {
+                box.checked = true;
+            }
+        });
+    }
+    lastChecked = this;
+};
+
+boxes.forEach(box => box.addEventListener('click', handleCheck));
