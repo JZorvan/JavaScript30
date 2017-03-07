873b3d35d9011d9021bc3780d2d39b44d9579fbc
Janelle Zorvan <jzorvan@gmail.com>
2017-03-06 22:49:40 -0600

    Finished Script

--- 09_14_Must_Know_Dev_Tools_Tricks/script.js
+++ 09_14_Must_Know_Dev_Tools_Tricks/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
+const dogs = [{ name: 'Snickers', age: 2 }, { name: 'Hugo', age: 8 }];
 
     function makeGreen() {
       const p = document.querySelector('p');
@@ -31,11 +31,37 @@ const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
     console.assert(p.classList.contains('zibbityza'), 'That is wrong.');
 
     // clearing
+    console.clear();
 
     // Viewing DOM Elements
+    console.log(p);
+    console.dir(p);
 
     // Grouping together
+    dogs.forEach(dog => {
+      console.groupCollapsed(`${dog.name}`);
+      console.log(`This is ${dog.name}.`);
+      console.log(`${dog.name} is ${dog.age} years old.`);
+      console.log(`${dog.name} is a good dog.`);
+      console.groupEnd(`${dog.name}`);
+    });
+
+    console.table(dogs);
 
     // counting
+    console.count('badger');
+    console.count('badger');
+    console.count('badger');
+    console.count('badger');
+    console.count('badger');
+    console.count('mushroom');
+    console.count('mushroom');
 
     // timing
+    console.time('fetching data');
+    fetch('https://api.github.com/users/JZorvan')
+      .then(data => data.json())
+      .then(data => {
+        console.timeEnd('fetching data');
+        console.log(data);
+      });
