785629332d67028520222e40b446428c56ba951a
Janelle Zorvan <jzorvan@gmail.com>
2017-03-13 08:24:30 -0500

    video play and pause

--- 11_Custom_HTML5_Video_Player/script.js
+++ 11_Custom_HTML5_Video_Player/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+/* Elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+/* Functions */
+
+function togglePlay() {
+    if(video.paused) {
+        const method = video.paused ? 'play' : 'pause';
+        video[method]();
+}
+
+function updateButton() {
+    
+}
+
+/* Event Listeners */
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', togglePlay);
+
+toggle.addEventListener('click', togglePlay);
+
